c311fa5446771855143ac9baa1d9df7e803d56f8
Jose Aviles <aviles.yanes7@gmail.com>
2022-10-24 19:44:10 -0600

    input updated

--- my-form/src/components/FormInput.js
+++ my-form/src/components/FormInput.js
@@ -1,11 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import "./formInput.css";
 
-const FormInput = ({ placeholder }) => {
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div>
-      {/* <label>Username</label> */}
-      <input placeholder={placeholder} />
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
   );
 };
